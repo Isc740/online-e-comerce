47878f86c112445bdea66c2632e24b9732a9484c
Isc740 <isaazcantillo@gmail.com>
2024-11-02 19:39:16 -0500

    change single product layout

--- src/single-product.js
+++ src/single-product.js
@@ -4,21 +4,26 @@ document.addEventListener("DOMContentLoaded", () => {
         sessionStorage.getItem("productIndex"),
     );
 
-    document.querySelector(".section").innerHTML += `
+    document.querySelector(".single-product").innerHTML += `
         <div class="product-container">
             <h2 class="product-title">${productData[productIndex].title}</h2>
             <img class="product-img" src="${productData[productIndex].image
         }" alt="product image">
             <div class="center-container">
-                <button class="product-btn">View Product</button>
+                <button class="btn-buy">Buy</button>
             </div>
             <p>Codigo: <strong>${productData[productIndex].id}</strong></p>
             <p class="product-desc">${productData[productIndex].description}</p>
             <p class="p-rate">Rating:<strong>${productData[productIndex].rating.rate
         }</strong></p>
-            <p class="p-rate">Amount bought:<strong>${productData[productIndex].rating.count
+            <p class="product-amount p-rate">Amount bought:<strong>${productData[productIndex].rating.count
         }</strong></p>
-            <p class="p-rate">Price:<strong class="product-price">$${productData[productIndex].price
+            <p class="p-rate">Price<strong class="product-price">$${productData[productIndex].price
         }</strong></p>
-        </div>`;
+            
+            <div class="center-container">
+                <button class="return-btn" onclick="fetchItems()">Return</button>
+            </div>
+        </div>
+    `;
 });
